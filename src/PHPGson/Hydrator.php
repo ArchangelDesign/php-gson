b1dbc431778d323d2484447f260cdb4b3354cc65
Raff <archangel.raffael@gmail.com>
2018-04-13 19:56:31 +0200

    Hydrator - initial work

---
+++ src/PHPGson/Hydrator.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PHPGson Library
+ * Simple entity mapper for PHP applications
+ * with minimum requirements and dependencies
+ *
+ * @author Rafal Martinez-Marjanski
+ * @package PHPGson
+ * @license MIT
+ */
+
+
+namespace PHPGson;
+
+/**
+ * Class Hydrator
+ * @package PHPGson
+ */
+class Hydrator
+{
+    public function __construct(array $properties)
+    {
+    }
+}
